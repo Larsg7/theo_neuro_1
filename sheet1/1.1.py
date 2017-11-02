33bd39ac347934ccea4dd441f6b816ade295d445
Lars Gröber <larsgroeber7@gmail.com>
2017-11-02 23:15:19 +0100

    added tex files

--- sheet1/1.1.py
+++ sheet1/1.1.py
@@ -2,29 +2,34 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 frequency = 100
-interval = 1000
+interval = 10000
 num_cells = 1
 
 spikes = []
 for i in range(num_cells):
-    isi = np.random.poisson(1000  / frequency, interval)
+    isi = np.random.poisson(1000 / frequency, interval)
     spikes.append(np.cumsum(isi))
 
+spikes = spikes[0]
 variation = np.std(isi)**2
 print("Variation: " + str(variation))
 print("Coefficient of variation: " + str(variation / (1000 / frequency)))
 
-max_spike_interval = 1000
-num_intervals = 10
-n_spikes = [0 for i in range(num_intervals)]
+for size in [1, 5, 10, 20, 50, 100]:
+    max_spike_interval = 10000
+    interval_size = size
+    interval_count = spikes[-1] // interval_size
+    n_spikes = [0 for i in range(interval_count)]
 
-for spike in spikes[0]:
-    index = (spike // (max_spike_interval // num_intervals))
-    if index < num_intervals:
-        n_spikes[index] += 1
+    for spike in spikes:
+        index = (spike // interval_size)
+        if index < interval_count:
+            n_spikes[index] += 1
 
-print("Fano: " + str(np.std(n_spikes) / 1))
+    print("Fano " + str(interval_size) + ": " + str(np.std(n_spikes)**2 / (interval_size / 100)))
 
-plt.hist(n_spikes, bins=np.arange(30), align='left', ec='black')
-#plt.hist(isi, bins=np.arange(30), align='left', ec='black')
-#plt.show()
+#plt.hist(n_spikes, bins=np.arange(30), align='left', ec='black')
+plt.hist(isi, bins=np.arange(30), align='left', ec='black')
+plt.xlabel("Interspike interval [ms]")
+plt.ylabel("Number of occurencies")
+plt.show()
